6eca476644e64832e7e7e52e936ab152a4c96dd5
tmt62 <70038684+tmt62@users.noreply.github.com>
2020-11-27 22:39:01 -0500

    Update Track_Controller_SW_Interface.java

--- Interfaces/Track_Controller_SW_Interface.java
+++ Interfaces/Track_Controller_SW_Interface.java
@@ -10,14 +10,24 @@ public interface Track_Controller_SW_Interface extends Remote {
     void make_Controllers(String[] lines, int[][] blocks) throws RemoteException;
 
     void train_Moved(int trainNum, int blockNum) throws RemoteException, FileNotFoundException; //Track Model -> Track Controller -> CTC
+
+    void set_Broken_Rail(int lineIndex, int blockNum, boolean state) throws RemoteException;
+
+    void set_Track_Circuit_Failure(int lineIndex, int blockNum, boolean state) throws RemoteException;
+
+    void set_Power_Fail(int lineIndex, int blockNum, boolean state) throws RemoteException;
     //Track Model calls these methods
 
     //CTC calls these methods
     void create_Train(int cars, String line, int block) throws RemoteException; // CTC -> track controller -> track model -> train model
 
-    void send_Speed_Authority(int train_Num, double speed, int authority) throws RemoteException; // CTC -> track controller -> track model -> train model
+    void remove_Train(int trainNum) throws RemoteException;
 
-    boolean set_Switch_Manual(String trackLine, int blockNum) throws RemoteException;
+    void send_Beacon_Information(int train_Num, String next_Stop) throws RemoteException;
+
+    void send_Speed_Authority(int trainNum, double speed, int authority) throws RemoteException; // CTC -> track controller -> track model -> train model
+
+    boolean set_Switch_Manual(String trackLine, int blockNum, boolean state) throws RemoteException;
 
     //static void add_Ticket(int trainNum) throws RemoteException;
 
@@ -28,9 +38,6 @@ public interface Track_Controller_SW_Interface extends Remote {
 
     //Hardware calls these methods
     void get_Block_Array(int start) throws RemoteException;
-    //Hardware calls these methods
-
-    //public void send_Block_State(int blockNum, boolean state) throws RemoteException;
     //Hardware calls these methods
 
     void update_Time(double time) throws RemoteException;
