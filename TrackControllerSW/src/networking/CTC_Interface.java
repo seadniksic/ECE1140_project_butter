95a6f65a3c44a6331e8d4583e6283ce5d938f9f4
tmt62 <70038684+tmt62@users.noreply.github.com>
2020-11-27 22:42:26 -0500

    Create CTC_Interface.java

---
+++ TrackControllerSW/src/networking/CTC_Interface.java
@@ -0,0 +1,27 @@
+package networking;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface CTC_Interface extends Remote{
+
+    public void close_Line(String line) throws RemoteException;//close entire track so each block is closed
+
+    public void close_Section(String line, char section) throws RemoteException;//close all blocks in section
+
+    public void close_Blocks(String line, char section, int startBlock, int endBlock) throws RemoteException;//close all blocks from start to stop
+
+    public void open_Line(String line) throws RemoteException;//close entire track so each block is closed
+
+    public void open_Section(String line, char section) throws RemoteException;//close all blocks in section
+
+    public void open_Blocks(String line, char section, int startBlock, int endBlock) throws RemoteException;//close all blocks from start to stop
+
+    public void train_Moved(int trainNum, int blockNum) throws RemoteException;//moves occupancy state of block of train
+
+    //public void import_Track_File(File trackFile) throws FileNotFoundException, RemoteException; // receives file from track model NOT IMPLEMENTED
+    public void add_Ticket_CTC(int trainNum) throws RemoteException;
+
+    public void update_Time(double time) throws RemoteException;//time of units in seconds
+}
